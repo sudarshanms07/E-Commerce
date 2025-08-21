b12fc48945a9ac3c69b55cd688e5c641aba30841
Sudarshan Maske <ms22maske@gmail.com>
2025-08-22 01:28:42 +0530

    changes for deployment

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -1,16 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import ShopContextProvider from "./context/ShopContext";
 
-
-
-
-
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-    <App />
-    
-    </BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>
+);
